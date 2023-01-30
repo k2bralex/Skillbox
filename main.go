080ad62d7d1e3221d3c863cfa38d54bdb69cf506
alex <k2.bralex@gmail.com>
2023-01-30 16:15:23 +0300

    refactor

--- main.go
+++ main.go
@@ -1,68 +1,78 @@
 package main
 
 import (
-	fnc "Skillbox/function"
+	/*	md4 "Skillbox/module_4"
+		md5 "Skillbox/module_5"
+		md6 "Skillbox/module_6"
+		md7 "Skillbox/module_7"
+		md8 "Skillbox/module_8"
+		md9 "Skillbox/module_9"
+		md10 "Skillbox/module_10"*/
+	md11 "Skillbox/module_11"
+
 	"fmt"
 )
 
 func main() {
 
-	/*	//Module 4
-		fnc.Ege()
-		fnc.GreaterThanFive()
-		fnc.ATM()
-		fnc.GreaterThanFiveCount()
-		fnc.CheckCalculator()
-		fnc.Students()
-
-		//Module 5
-		fnc.Qarter()
-		fnc.IsAnyPositive()
-		fnc.IsDistinct()
-		fnc.NoChange()
-		fnc.QuadraticEquationRoots()
-		fnc.TicketCheck()
-		fnc.GuessNumber()
-
-		//Module 6
-		fnc.IntSequence()
-		fnc.SumByOne()
-		fnc.Discount()
-		fnc.LimitsCount()
-		fnc.Apples()
-		fnc.Elevator()
-
-		//Module 7
-		fnc.MirrorTicketCount()
-		fnc.ChessBoard()
-		fnc.FirTree()
-		fnc.MaxDistance()
-
-		//Module 8
-		fnc.Seasons()
-		fnc.WorkingDays()
-		fmt.Println(fnc.GetChange([]int{5, 5, 5, 10, 20}))
-		fmt.Println(fnc.GetChange([]int{10, 10}))
-		fmt.Println(fnc.GetChange([]int{5, 5, 10, 10, 20}))
-
-		//Module 9
-		fnc.OverflowCount()
-		fmt.Println(fnc.MinType(1, 1))
-		fmt.Println(fnc.MinType(1, -1))
-		fmt.Println(fnc.MinType(640, 100))
-		fmt.Println(fnc.MinType(-640, 100))
-		fmt.Println(fnc.MinType(3000, 3000))
-		fmt.Println(fnc.MinType(-3000, 3000))
-
-		//Module 10
-		fnc.Tailor()
-		fnc.Accountant()*/
+	//Module 12
 
 	//Module 11
 	exp := "Go is an Open source programming Language that makes it Easy to build simple, " +
 		"reliable, and efficient Software"
-	fmt.Println(fnc.CapitalWordsCount(exp))
+	fmt.Println(md11.CapitalWordsCount(exp))
 
 	exp = "a10 10 20b 20 30c30 30 dd"
-	fmt.Println(fnc.GetDigits(exp))
+	fmt.Println(md11.GetDigits(exp))
+
+	/*//Module 10
+	md10.Tailor()
+	md10.Accountant()
+
+	//Module 9
+	md9.OverflowCount()
+	fmt.Println(md9.MinType(1, 1))
+	fmt.Println(md9.MinType(1, -1))
+	fmt.Println(md9.MinType(640, 100))
+	fmt.Println(md9.MinType(-640, 100))
+	fmt.Println(md9.MinType(3000, 3000))
+	fmt.Println(md9.MinType(-3000, 3000))
+
+	//Module 8
+	md8.Seasons()
+	md8.WorkingDays()
+	fmt.Println(md8.GetChange([]int{5, 5, 5, 10, 20}))
+	fmt.Println(md8.GetChange([]int{10, 10}))
+	fmt.Println(md8.GetChange([]int{5, 5, 10, 10, 20}))
+
+	//Module 7
+	md7.MirrorTicketCount()
+	md7.ChessBoard()
+	md7.FirTree()
+	md7.MaxDistance()
+
+	//Module 6
+	md6.IntSequence()
+	md6.SumByOne()
+	md6.Discount()
+	md6.LimitsCount()
+	md6.Apples()
+	md6.Elevator()
+
+	//Module 5
+	md5.Qarter()
+	md5.IsAnyPositive()
+	md5.IsDistinct()
+	md5.NoChange()
+	md5.QuadraticEquationRoots()
+	md5.TicketCheck()
+	md5.GuessNumber()
+
+	//Module 4
+	md4.Ege()
+	md4.GreaterThanFive()
+	md4.ATM()
+	md4.GreaterThanFiveCount()
+	md4.CheckCalculator()
+	md4.Students()*/
 }
